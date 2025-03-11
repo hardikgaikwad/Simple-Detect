c1e7fec68d8a87ea3288ce40e845af20ab793816
hardikgaikwad <hardikgaikwad04@gmail.com>
2025-03-11 19:07:19 +0530

    Fixed the live or file capture logic

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 import pyshark
 import argparse
+import os
 from packet_sniffer import cap_pkt
 
-parser = argparse.Argumentparser()
-group_pk_snfr = parser.add_mutually_exclusive_group(required=True)
-group_pk_snfr.add_argument('-l', '--live', type=str, help="It is used to initiate Live Capture of the packets")
-group_pk_snfr.add_argument('-p', '--pcap', type=str, help="It is used to read from the PCAP files")
-args = parser.parse_args()
 
-cap_pkt(args.live, args.pcap)
+def main():
+    parser = argparse.Argumentparser()                                                                                          # Create an argument parser
+    group1 = parser.add_mutually_exclusive_group(required=True)
+    group1.add_argument('-l', '--live', type=str, help=""": used to capture live packets if you do not have an existing PCAP file.
+                                                            interface, output path and count flags are to be used while capturing live packets.""")
+    group1.add_argument('-f', '--file', type=str, help=": used to store path to an existing PCAP file.")
+
+    parser.add_argument('-i', '--interface', type=str, help="It is used to specify the interface type")
+    parser.add_argument('-o', '--output', type=str, default='capture.pcap', help="It is the output of the file capture stored")
+    parser.add_argument('-c', '--count', type=int, default=10, help="It is the number of packets to be stored in the PCAP file")
+    args = parser.parse_args()
+
+
+    if args.file:
+        if os.path.exists(args.file):
+            print(f"File Found. Using PCAP file: {args.file}")
+            pcap_file = args.file
+        else:
+            print("File not found")
+    elif args.live:
+        cap_pkt(interface=args.interface, output_path=args.output, packet_limit=args.count))
+
+if __name__ = "__main__":
--- packet_sniffer.py
+++ packet_sniffer.py
@@ -1,13 +1,22 @@
 import pyshark
+import os
 
-def cap_pkt(interface=None, file_path=None):
-    if file_path:
-        capture = pyshark.FileCapture(file_path)
-        print("Sniffing Packets from File...")
-    else:
-        capture = pyshark.LiveCapture(interface=interface)
-        print("Sniffing Live Packets...")
+#ef cap_pkt(interface=None, file_path=None): 
+#   if file_path:
+#       capture = pyshark.FileCapture(file_path)                                        # Capture packets from a PCAP file
+#       print("Sniffing Packets from File...")
+#   else:
+#       capture = pyshark.LiveCapture(interface=interface)                              # Capture Live Packets from the network interface
+#       print("Sniffing Live Packets...")
 
-    for pkt in capture.sniff_continuously(packet_count=5):
-        print(pkt)
+#   for pkt in capture.sniff_continuously(packet_count=5):                              # Capture 5 packets continuously
+#       print(pkt)
+
+def cap_pkt(interface='eth0', output_path, packet_limit=10):
+    
+    print("Capturing live packets...")
+
+    cap = pyshark.LiveCapture(interface=interface, output_file=output_path)
+    cap.sniff_continuously(packet_count=packet_limit)
+    print("Capture Complete")
     
